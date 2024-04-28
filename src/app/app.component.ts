66d6c2fdc0bbf7eb71f5309609eb55c8dc77011a
nkl708 <nklnsk708@gmail.com>
2024-04-29 01:08:33 +0700

    Use CommonModule
    An error occured because the BrowserModule is only used in NgModule (not
    in Standalone)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
 import { Component } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { FooterComponent } from "./components/footer/footer.component";
 import { HeaderComponent } from "./components/header/header.component";
-import { MainComponent } from "./components/main/main.component";
 import { ThemeToggleComponent } from "./components/theme-toggle/theme-toggle.component";
-import { HttpClientModule } from "@angular/common/http";
+
+import { MainComponent } from "./components/main/main.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
   imports: [
-    BrowserModule,
+    CommonModule,
     NgbModule,
     HttpClientModule,
 
